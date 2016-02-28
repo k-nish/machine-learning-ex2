90608624e45f739dbf8433141ef938b944d5c8a8
Kouhei Nishimura <koh.oct.un@gmail.com>
2016-02-28 17:23:03 +0800

    costFunctionまで終了

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -20,12 +20,12 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+%J = (-1 / m) * sum( y(i) * log(g(theta.' * X(i,:))) + (1-y(i)) * log(1-g(theta.' * X(i,:))));
 
+h = sigmoid(X * theta);
 
-
-
-
-
+J = (1 / m) * sum(-y .* log(h) - (1 - y) .* log(1 - h));
+grad = (1 / m) * sum((h - y) .* X);
 
 % =============================================================
 
