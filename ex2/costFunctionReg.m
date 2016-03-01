13a9025766e07aa824fae1e7f4b9319fb2a13247
Kouhei Nishimura <koh.oct.un@gmail.com>
2016-03-01 22:38:32 +0800

    一応課題を全部終了 warning: product: automatic broadcasting operation appliedのエラーを修正しないと

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -2,12 +2,12 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
 %   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -17,10 +17,20 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+%h = sigmoid(X * theta);
 
+%J = (1 / m) * sum(-y .* log(h) - (1 - y) .* log(1 - h)) + (lambda / 2 * m) * sum ((theta).^2);
 
+%if j = 0
+%	grad = (1 / m) * sum((h - y) .* X);
+%if j >= 1
+%    grad = (1 / m) * sum((h - y) .* X) + (lambda / m) * theta;
+%endif
 
-
+h = sigmoid(X * theta);
+thetaExcludingZero = [ [ 0 ]; theta([2:length(theta)])];
+J = (1 / m) * sum(-y .* log(h) - (1 - y) .* log(1 - h)) + (lambda / (2 * m)) * sum(thetaExcludingZero .^ 2);
+grad = (1 / m) * sum((h - y) .* X) + (lambda / m) * thetaExcludingZero';
 
 % =============================================================
 
